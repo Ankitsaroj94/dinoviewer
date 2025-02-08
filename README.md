47f2e74edc7ae93e97fe8d4f9cc91ce60ecd764e
Ankit <Ankit.saroj@voxturrlabs.com>
2025-02-08 13:13:36 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dinoviewer
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/dinoviewer/dinoviewer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dinoviewer.dinoviewer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
